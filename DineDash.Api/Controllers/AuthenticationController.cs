a7505f0ee08f938a43f6de0574ca3115bc784cb7
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-10-14 05:29:56 +0300

    Basic Controller

---
+++ DineDash.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+using DineDash.Application.Services.Authentication;
+using DineDash.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DineDash.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var result = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+        );
+
+        var response = new AuthenticationResponse(
+            result.Id,
+            result.FirstName,
+            result.LastName,
+            result.Email,
+            result.Token
+        );
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var result = _authenticationService.Login(request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            result.Id,
+            result.FirstName,
+            result.LastName,
+            result.Email,
+            result.Token
+        );
+
+        return Ok(response);
+    }
+}
