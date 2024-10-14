895d01a3c082080ae05c5a54b5f247cce2f1a68b
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-10-14 05:30:38 +0300

    Feat: Infrastructure Layer Setup w/ Dependency Injection Extension

---
+++ DineDash.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using DineDash.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DineDash.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        // Register Infra Services
+
+        return services;
+    }
+}
