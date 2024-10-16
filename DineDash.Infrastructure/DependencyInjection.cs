5b426f5714a76a42e89edd0fc6aca4bc04a4e675
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-10-16 06:48:22 +0300

    feat: configure Infrastructure layer services including JwtSettings, IJwtTokenGenerator, and IDateTimeProvider

--- DineDash.Infrastructure/DependencyInjection.cs
+++ DineDash.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,23 @@
+using DineDash.Application.Common.Interfaces.Authentication;
+using DineDash.Application.Common.Interfaces.Services;
 using DineDash.Application.Services.Authentication;
+using DineDash.Infrastructure.Authentication;
+using DineDash.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DineDash.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfigurationManager configuration
+    )
     {
-        // Register Infra Services
-
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
