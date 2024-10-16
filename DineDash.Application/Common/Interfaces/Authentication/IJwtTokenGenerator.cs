01284b3e411b0387292fc31bc1a4b340247eac90
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-10-16 06:41:01 +0300

    Chore: Create IJwtTokenGenerator Interface

---
+++ DineDash.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,6 @@
+namespace DineDash.Application.Common.Interfaces.Authentication;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(Guid userId, string firstName, string lastName);
+}
