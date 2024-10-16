fcbd10fecd3be807e9d0ccf64deac7a25b332653
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-10-16 06:40:26 +0300

    Chore: Configure Application and Infrastructure DI into Services

--- DineDash.Api/Program.cs
+++ DineDash.Api/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 {
-    builder.Services.AddApplication().AddInfrastructure();
+    builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
 }
 
